885cb58e0e42a5b46a59fe513053720b10f26279
Yanxin Gong <yanxin.gong22@gmail.com>
2013-01-02 21:52:12 +0100

    init version

---
+++ QQExternalKiller.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# watchdog
+#
+# this watchdog is to kill QQExternal.exe each miniut
+# crontab -e
+# * * * * * /usr/sbin/watchdog
+
+NAME=QQExternal
+
+ps ax | grep -v grep |grep $NAME | grep -oP "(?<= )[0-9]+ "
+case $? in
+	0)
+		ps ax | grep -v grep |grep $NAME | grep -oP "(?<= )[0-9]+ " | xargs kill
+	;;
+esac
+
+exit
